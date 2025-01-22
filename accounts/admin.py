b425f95690a152e00fe80afcbe76606aa3a16875
Jilles S <jilleswf@gmail.com>
2025-01-22 10:29:36 -0600

    Add fieldsets for user creation in the admin panel.

--- accounts/admin.py
+++ accounts/admin.py
@@ -15,5 +15,16 @@ class CustomUserAdmin(UserAdmin):
         "email",
     )
 
+    # The fields that will be displayed in the admin panel when adding a new user
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "first_name", "last_name", "password1", "password2"),
+            },
+        ),
+    )
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
